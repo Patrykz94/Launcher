26f68507a752f251b543ef1440d9d4b573a9a24a
Patryk Zawierucha <zawpat@googlemail.com>
2018-10-31 19:01:46 +0000

    Start working on dynamically displaying game buttons and information about games using XML files

--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -22,8 +22,86 @@ namespace Launcher
             LocationsManager locations = new LocationsManager();
         }
 
+        public void PopulateLibrary()
+        {
+            string gameLocations = LocationsManager.dataDir + "\\libFiles";
+            string gameLocationsFile = gameLocations + "\\main.xml";
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(gameLocationsFile);
+                XmlNode root = doc.FirstChild;
+                XmlNodeList nodeList = root.SelectNodes("descendant::download");
+                List<string> downloads = new List<string>();
+
+                foreach (XmlNode n in nodeList)
+                {
+                    if (n.HasChildNodes)
+                    {
+                        foreach (XmlNode c in n.ChildNodes)
+                        {
+                            if (c.Name == "infoFile")
+                            {
+                                downloads.Add(c.InnerText);
+                            }
+                        }
+                    }
+                }
+
+                int i = 0;
+                foreach (Button b in Sidebar.Controls.OfType<Button>())
+                {
+                    if (b.Name.StartsWith("ButtonApp"))
+                    {
+                        Sidebar.Controls.Remove(b);
+                    }
+                }
+
+                foreach (string d in downloads)
+                {
+                    XmlDocument game = new XmlDocument();
+                    try
+                    {
+                        game.Load(gameLocations + d);
+                        string title = game.DocumentElement.SelectSingleNode("title").InnerText;
+                        string iconPath = gameLocations + game.DocumentElement.SelectSingleNode("icon").InnerText;
+
+                        Button btn = new Button();
+                        btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+                        btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+                        btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(88)))), ((int)(((byte)(88)))));
+                        btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
+                        btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                        btn.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+                        btn.Image = Image.FromFile(iconPath);
+                        btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+                        btn.Location = new System.Drawing.Point(0, i * 40);
+                        btn.Margin = new System.Windows.Forms.Padding(0);
+                        btn.Name = "ButtonApp" + i.ToString();
+                        btn.Size = new System.Drawing.Size(200, 40);
+                        btn.Text = "  " + title;
+                        btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+                        btn.UseVisualStyleBackColor = false;
+                        btn.Click += new EventHandler(ButtonApp_Click);
+                        Sidebar.Controls.Add(btn);
+                        i++;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
         private void LauncherForm_Load(object sender, EventArgs e)
         {
+            PopulateLibrary();
             // Once loaded, check for updates
         }
 
@@ -69,6 +147,37 @@ namespace Launcher
                 SettingsScreen settings = new SettingsScreen();
                 settings.Show();
             }
+        }
+
+        private void ButtonApp_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            // Find the game description form and replace with the one corresponding to the button clicked
+            foreach (MainGamePanel p in Form.ActiveForm.Controls.OfType<MainGamePanel>())
+            {
+                if (p.Name == "MainGamePanel")
+                {
+                    Form.ActiveForm.Controls.Remove(p);
+                }
+            }
+
+            string gameInfoFile = LocationsManager.dataDir + "\\libFiles\\downloads\\" + button.Text.Substring(2) + "\\info.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(gameInfoFile);
+
+            MainGamePanel mainPanel = new MainGamePanel();
+
+            mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            MainPanelHolder.Controls.Add(mainPanel);
+
+            // Set title of the game
+            Control[] LabelGameName = mainPanel.Controls.Find("LabelGameName", true);
+            LabelGameName[0].Text = doc.DocumentElement.SelectSingleNode("title").InnerText;
+
+            // Set title of the game
+            Control[] LabelDescriptionText = mainPanel.Controls.Find("LabelDescriptionText", true);
+            LabelDescriptionText[0].Text = doc.DocumentElement.SelectSingleNode("description").InnerText;
         }
     }
 }
